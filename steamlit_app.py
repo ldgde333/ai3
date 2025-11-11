603aee823d60c55a36d596f0170e4e563c3efcb7
ldgde333 <h098s02815@chunchon.gwe.hs.kr>
2025-11-11 14:29:49 +0900

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -74,6 +74,19 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     #   "images": ["https://www.bhc.co.kr/upload/bhc/menu/%EC%96%91%EB%85%90%EC%B9%98%ED%82%A8_%EC%9C%99_410x271.jpg"],
        "videos": ["https://www.youtube.com/watch?v=UjSTTH7pbOc"]
      },
+
+
+ labels[1]: {
+       "texts": ["치킨은 맛있다."],
+    #   "images": ["https://www.bhc.co.kr/upload/bhc/menu/%EC%96%91%EB%85%90%EC%B9%98%ED%82%A8_%EC%9C%99_410x271.jpg"],
+       "videos": ["https://www.youtube.com/watch?v=UjSTTH7pbOc"]
+     },
+
+ labels[2]: {
+       "texts": ["치킨은 맛있다."],
+    #   "images": ["https://www.bhc.co.kr/upload/bhc/menu/%EC%96%91%EB%85%90%EC%B9%98%ED%82%A8_%EC%9C%99_410x271.jpg"],
+       "videos": ["https://www.youtube.com/watch?v=UjSTTH7pbOc"]
+     },
 }
 
 # ======================
